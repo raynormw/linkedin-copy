d59efac7c380ad18b532f6da27737a6ad3a78672
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2019-03-16 02:01:28 +0700

    :hammer: Setting up primary variable

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ class App extends Component {
           >
             Learn React
           </a>
+          <button type="button" className="btn btn-primary">Primary</button>
+          <button type="button" className="btn btn-warning">Warning</button>
+          <button type="button" className="btn btn-success">Success</button>
+          <button type="button" className="btn btn-outline-primary">Outlined Primary</button>
         </header>
       </div>
     );
