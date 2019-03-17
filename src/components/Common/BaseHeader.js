767af26c16184a36ddce5cc1030bacd516323064
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2019-03-17 07:05:37 +0700

    :construction: WIP BaseHeader

--- src/components/Common/BaseHeader.js
+++ src/components/Common/BaseHeader.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import BaseButton from 'components/Common/BaseButton';
 
 class BaseHeader extends Component {
+  state = {
+    urls: [
+      { text: "Home", link: "/", className: "fa fa-home fa-2x", linkClass: "nav-url-active"},
+      { text: "My Network", link: "/", className: "nav-item text-center lh-8"},
+      { text: "Jobs", link: "/", className: "nav-item text-center lh-8"},
+      { text: "Messaging", link: "/", className: "nav-item text-center lh-8"},
+      { text: "Notifications", link: "/", className: "nav-item text-center lh-8"},
+    ]
+  }
+
   render() {
     return (
       <nav className="navbar fixed-top base-header pl-5 pr-5">
@@ -18,11 +29,21 @@ class BaseHeader extends Component {
           </div>
         </form>
         <ul className="nav mr-2">
-          <li className="nav-item">
-            <a className="nav-link active" href="#">Active</a>
+          <li className="nav-item text-center lh-8">
+            <Link to="/" type="button" className="nav-url-active">
+              <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+              <p className="mb-0"><small>Home</small></p>
+            </Link>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
+          <li className="nav-item text-center lh-8">
+            <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+            <p className="mb-0"><small>Home</small></p>
+          </li>
+          <li className="nav-item text-center lh-8">
+            <Link to="/" type="button" className="nav-url">
+              <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+              <p className="mb-0"><small>Home</small></p>
+            </Link>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">Link</a>
