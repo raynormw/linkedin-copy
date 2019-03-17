9d579608ab48850b6c419904185df84b99f57910
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2019-03-17 18:51:56 +0700

    :sparkles: NavItem component done

--- src/components/Common/Header/BaseHeader.js
+++ src/components/Common/Header/BaseHeader.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
 import BaseButton from 'components/Common/BaseButton';
+import NavItem from 'components/Common/Header/NavItem';
 
 class BaseHeader extends Component {
   state = {
-    urls: [
+    items: [
       { text: "Home", link: "/", className: "fa fa-home fa-2x", linkClass: "nav-url-active"},
       { text: "My Network", link: "/", className: "fa fa-home fa-2x", linkClass: "nav-url"},
       { text: "Jobs", link: "/", className: "fa fa-home fa-2x", linkClass: "nav-url"},
@@ -14,28 +15,23 @@ class BaseHeader extends Component {
   }
 
   render() {
-    let urls = (
-      <ul className="nav mr-2">
-        {
-          this.state.urls.map(url => {
-            return (
-              <li className="nav-item text-center lh-8">
-                <BaseButton to={url.link} type="button" className={url.linkClass}>
-                  <i className="fa fa-home fa-2x" aria-hidden="true"></i>
-                  <p className="mb-0"><small>{url.text}</small></p>
-                </BaseButton>
-              </li>
-            )
-          })
-        }
-      </ul>
-    );
+    let items = this.state.items.map((item, index) => {
+      return (
+        <NavItem
+          key={index}
+          text={item.text}
+          link={item.link}
+          className={item.className}
+          linkClass={item.linkClass}
+        />
+      )
+    });
 
     return (
       <nav className="navbar fixed-top base-header pl-5 pr-5">
         <form className="form-inline ml-4">
           <BaseButton to="/" type="button" className="btn btn-primary icon-li">
-            <i class="fa fa-linkedin fa-2x"></i>
+            <i className="fa fa-linkedin fa-2x"></i>
           </BaseButton>
           <div className="input-group">
             <input type="text" className="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1" />
@@ -45,7 +41,25 @@ class BaseHeader extends Component {
           </div>
         </form>
         <ul className="nav mr-2">
-          {urls}
+          {items}
+          <li className="nav-item text-center lh-8">
+            <BaseButton to="/" type="button" className="nav-url">
+              <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+              <p className="mb-0"><small>Navitestes</small></p>
+            </BaseButton>
+          </li>
+          <li className="nav-item text-center lh-8">
+            <BaseButton to="/" type="button" className="nav-url">
+              <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+              <p className="mb-0"><small>Navitestes</small></p>
+            </BaseButton>
+          </li>
+          <li className="nav-item text-center lh-8">
+            <BaseButton to="/" type="button" className="nav-url">
+              <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+              <p className="mb-0"><small>Navitestes</small></p>
+            </BaseButton>
+          </li>
         </ul>
       </nav>
     );
--- src/components/Common/Header/NavItem.js
+++ src/components/Common/Header/NavItem.js
@@ -5,7 +5,7 @@ import BaseButton from 'components/Common/BaseButton';
 const NavItem = (props) => {
   return (
     <li className="nav-item text-center lh-8">
-      <BaseButton to={props.url} type="button" className={props.linkClass}>
+      <BaseButton to={props.link} type="button" className={props.linkClass}>
         <i className={props.className} aria-hidden="true"></i>
         <p className="mb-0"><small>{props.text}</small></p>
       </BaseButton>
