f1f7f514142a65c3cfedc32f8ed2de18bbf252fb
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2019-03-18 19:21:09 +0700

    :hammer: Setting up layout

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -10,6 +10,35 @@ class Homepage extends Component {
       <div className="App">
         <BaseHeader />
         <div className="base-content">
+          <div className="column-1">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div className="column-2 px-3">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div className="column-3">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="base-content">
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -45,29 +74,6 @@ class Homepage extends Component {
           <button type="button" className="btn btn-success">Success</button><br/><br/>
           <BaseButton to="/profile" type="button" className="btn btn-outline-primary">
             <i className="fa fa-times mr-2"></i>
-            Go to Profile
-          </BaseButton>
-        </div>
-        <div className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <br/>
-          <BaseButton type="button" className="btn btn-primary">
-            Primary
-          </BaseButton>
-          <br/><br/>
-          <button type="button" className="btn btn-warning rounded-0">Warning</button><br/><br/>
-          <button type="button" className="btn btn-success">Success</button><br/><br/>
-          <BaseButton to="/profile" type="button" className="btn btn-outline-primary">
             Go to Profile
           </BaseButton>
         </div>
