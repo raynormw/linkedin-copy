3f1b4cb05273098ecd7735b6b5ca00e1c90ae887
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2019-03-18 22:45:22 +0700

    :sparkles: Recommend Card component done

--- src/components/Homepage/RecommendCard.js
+++ src/components/Homepage/RecommendCard.js
@@ -5,10 +5,44 @@ import BaseButton from 'components/Common/BaseButton';
 
 class RecommendCard extends Component {
   state = {
-    
+    lists: [
+      { name: "Max Levchin", title: "Co-Founder & CEO at Affirm, Inc" },
+      { name: "Shopee", title: "Company - Internet" },
+      { name: "Software Engineering", title: "Topic" }
+    ]
   };
 
   render() {
+    let lists = this.state.lists.map((list, index) => {
+      return (
+        <div className="row align-items-center my-2 pl-1">
+          <div className="col-md-3 pr-1">
+            <ImageRounded
+              className="rounded-circle"
+              src="http://i.pravatar.cc/48"
+              alt="avatar-feed"
+              width="48"
+              height="48"
+            />
+          </div>
+          <div className="col-md-5 p-0">
+            <p className="font-weight-bold mb-0">{list.name}</p>
+            <p className="text-muted mb-0">{list.title}</p>
+          </div>
+          <div className="col-md-4 pl-1">
+            <BaseButton
+              to="/"
+              type="button"
+              className="btn btn-outline-dark rounded-0 py-1 fs-12"
+            >
+              <i className="fa fa-plus mr-2"></i>
+              Follow
+            </BaseButton>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div className="card rounded-0 shadow">
         <div className="card-body p-2">
@@ -18,31 +52,7 @@ class RecommendCard extends Component {
             </p>
             <i className="fa fa-info-circle fa-lg" aria-hidden="true"></i>
           </div>
-          <div className="row align-items-center my-2 pl-1">
-            <div className="col-md-3 pr-1">
-              <ImageRounded
-                className="rounded-circle"
-                src="http://i.pravatar.cc/48"
-                alt="avatar-feed"
-                width="48"
-                height="48"
-              />
-            </div>
-            <div className="col-md-5 p-0">
-              <p className="font-weight-bold mb-0">Max Levchin</p>
-              <p className="text-muted mb-0">Co-Founder & CEO at Affirm, Inc</p>
-            </div>
-            <div className="col-md-4 pl-1">
-              <BaseButton
-                to="/"
-                type="button"
-                className="btn btn-outline-dark rounded-0 py-1 fs-12"
-              >
-                <i className="fa fa-plus mr-2"></i>
-                Follow
-              </BaseButton>
-            </div>
-          </div>
+          {lists}
           <a href="/" className="pl-1">View all recommendations</a>
         </div>
       </div>
